38d737a635fa43d375d832ddc89026848ee7991f
zdying <zdying@live.com>
2017-06-29 16:27:05 +0800

    fix bug; update code style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@
  * @author zdying
  * @providesModule ActionSheet
  */
-import React, {Component} from 'react';
-import {
-    Modal, Text, View, Animated, TouchableWithoutFeedback, TouchableOpacity,
-    Platform, ActionSheetIOS, StyleSheet, Button
-} from 'react-native';
+import React, { Component } from 'react';
+import { Modal, Text, View, Animated, TouchableWithoutFeedback, TouchableOpacity, Platform, ActionSheetIOS } from 'react-native';
 import styles from './styles';
 
 const isIOS = Platform.OS === 'ios';
@@ -16,179 +13,191 @@ const BUTTON_HEIGHT = 58;
 const GROUP_SPACE_WIDTH = 10;
 
 export default class ActionSheet extends Component {
-    constructor(props, state){
-        super(props, state);
+  constructor (props, state) {
+    super(props, state);
 
-        global.__action_sheet = this;
-        this.state = {
-            sheets: [],
-            marginBottomValue: new Animated.Value(0)
-        };
+    if (!global.__action_sheet) {
+      global.__action_sheet = this;
+      this.state = {
+        sheets: [],
+        marginBottomValue: new Animated.Value(0)
+      };
 
-        this.state.marginBottomValue.addListener(({value}) => this._value = value);
+      this.state.marginBottomValue.addListener(({value}) => { this._value = value; });
+    } else {
+      this.__should_not_render = true;
     }
+  }
 
-    show(opts, callback){
-        let sheets = this.state.sheets;
-        let height = this.getHeight(opts);
+  show (opts, callback) {
+    let sheets = this.state.sheets;
+    let height = this.getHeight(opts);
 
-        this.state.marginBottomValue.setValue(-height);
+    this.state.marginBottomValue.setValue(-height);
 
-        this.setState({
-            sheets: [...sheets, {
-                opts,
-                callback
-            }],
-            height
-        })
-    }
-
-    hide(){
-        Animated.timing(
-            this.state.marginBottomValue,
-            {toValue: -this.state.height, duration: 210, delay: 0}
-        ).start(()=>{
-            this.setState({
-                sheets: this.state.sheets.slice(0, -1)
-            });
-        });
-    }
-
-    getHeight(opts){
-        let {title, cancelButtonIndex, options, destructiveButtonIndex} = opts;
-
-        let height = options.length * BUTTON_HEIGHT + 10;
-
-        if(title){
-            height += TITLE_HEIGHT;
+    this.setState({
+      sheets: [
+        ...sheets,
+        {
+          opts,
+          callback
         }
+      ],
+      height
+    });
+  }
 
-        if(cancelButtonIndex < options.length && cancelButtonIndex !== destructiveButtonIndex){
-            height += GROUP_SPACE_WIDTH;
-        }
+  hide () {
+    Animated.timing(
+      this.state.marginBottomValue,
+      {toValue: -this.state.height, duration: 210, delay: 0}
+    ).start(() => {
+      this.setState({
+        sheets: this.state.sheets.slice(0, -1)
+      });
+    });
+  }
 
-        return height
+  getHeight (opts) {
+    let {title, cancelButtonIndex, options, destructiveButtonIndex} = opts;
+
+    let height = options.length * BUTTON_HEIGHT + 10;
+
+    if (title) {
+      height += TITLE_HEIGHT;
     }
 
-    render(){
-        if(/*isIOS || */this.state.sheets.length === 0){
-            return null
-        }
-
-        Animated.timing(
-            this.state.marginBottomValue,
-            {toValue: 0, duration: 210, delay: 10}
-        ).start();
-
-        let onMaskPress = this.onMaskPress.bind(this);
-
-        return (
-            <Modal visible={true} transparent={true} animationType="fade" onRequestClose={onMaskPress}>
-                <TouchableWithoutFeedback onPress={onMaskPress}>
-                    <Animated.View style={[
-                        styles.main, {marginBottom: this.state.marginBottomValue}
-                    ]}>
-                        <View style={styles.body}>
-                            {this.renderActionButtons()}
-                        </View>
-                    </Animated.View>
-                </TouchableWithoutFeedback>
-            </Modal>
-        )
+    if (cancelButtonIndex < options.length && cancelButtonIndex !== destructiveButtonIndex) {
+      height += GROUP_SPACE_WIDTH;
     }
 
-    renderActionButtons(){
-        let {sheets} = this.state;
-        let lastSheet = sheets.slice(-1)[0];
-        let {buttonList, cancelButton, title} = this.parseOpitons(lastSheet);
+    return height;
+  }
 
-        return [
-            this.renderButtonList(buttonList, {}, title),
-            cancelButton && this.renderButtonList([cancelButton], {marginTop: 10})
-        ]
+  render () {
+    if (this.__should_not_render || this.state.sheets.length === 0) {
+      return null;
     }
 
-    parseOpitons(sheet){
-        let {opts, callback} = sheet;
-        let buttonList = [];
-        let cancelButton = null;
-        let {title, options=[], tintColor='#157EFB', cancelButtonIndex, destructiveButtonIndex} = opts;
+    Animated.timing(
+      this.state.marginBottomValue,
+      {toValue: 0, duration: 210, delay: 10}
+    ).start();
 
-        (options).forEach((option, index) => {
-            let btnObj = {
-                title: option,
-                index,
-                callback
-            };
+    let onMaskPress = this.onMaskPress.bind(this);
 
-            if(index === destructiveButtonIndex){
-                btnObj.color = 'red';
-            }else{
-                btnObj.color = tintColor;
-            }
-
-            if(index === cancelButtonIndex && cancelButtonIndex !== destructiveButtonIndex){
-                btnObj.fontWeight = 'bold';
-                cancelButton = btnObj;
-            }else{
-                btnObj.fontWeight = 'normal';
-                buttonList.push(btnObj)
-            }
-        });
-
-        return {
-            buttonList,
-            cancelButton,
-            title
-        }
-    }
-
-    renderButtonList(list, style, title){
-        return (
-            <View style={[styles.buttonContainer, style]} key={"button_list_" + Math.random()}>
-                {title ? <View style={styles.header}><Text style={styles.headerText}>{title}</Text></View> : null}
-                {
-                    list.map((button, bIndex) => {
-                        let {index, title, color, fontWeight, callback} = button;
-                        let borderBottomWidth = Number(bIndex !== list.length - 1);
-
-                        return (
-                            <TouchableOpacity
-                                style={[styles.button, {borderBottomWidth}]}
-                                key={'button_' + button.index}
-                                onPress={this.onButtonPress.bind(this, index, callback)}
-                                activeOpacity={0.9}
-                            >
-                                <Text style={[styles.buttonText, {color, fontWeight}]}>{title}</Text>
-                            </TouchableOpacity>
-                        )
-                    })
-                }
+    return (
+      <Modal
+        visible={true}
+        transparent={true}
+        animationType='fade'
+        onRequestClose={onMaskPress}>
+        <TouchableWithoutFeedback onPress={onMaskPress}>
+          <Animated.View style={[ styles.main, {marginBottom: this.state.marginBottomValue} ]}>
+            <View style={styles.body}>
+              {this.renderActionButtons()}
             </View>
-        );
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  }
+
+  renderActionButtons () {
+    let {sheets} = this.state;
+    let lastSheet = sheets.slice(-1)[0];
+    let {buttonList, cancelButton, title} = this.parseOpitons(lastSheet);
+
+    return [
+      this.renderButtonList(buttonList, {}, title),
+      cancelButton && this.renderButtonList([cancelButton], {marginTop: 10})
+    ];
+  }
+
+  parseOpitons (sheet) {
+    let {opts, callback} = sheet;
+    let buttonList = [];
+    let cancelButton = null;
+    let {title, options = [], tintColor = '#157EFB', cancelButtonIndex, destructiveButtonIndex} = opts;
+
+    (options).forEach((option, index) => {
+      let btnObj = {
+        title: option,
+        index,
+        callback
+      };
+
+      if (index === destructiveButtonIndex) {
+        btnObj.color = 'red';
+      } else {
+        btnObj.color = tintColor;
+      }
+
+      if (index === cancelButtonIndex && cancelButtonIndex !== destructiveButtonIndex) {
+        btnObj.fontWeight = 'bold';
+        cancelButton = btnObj;
+      } else {
+        btnObj.fontWeight = 'normal';
+        buttonList.push(btnObj);
+      }
+    });
+
+    return {
+      buttonList,
+      cancelButton,
+      title
+    };
+  }
+
+  renderButtonList (list, style, title) {
+    return (
+      <View style={[styles.buttonContainer, style]} key={'button_list_' + Math.random()}>
+        {title ? <View style={styles.header}>
+          <Text style={styles.headerText}>
+            {title}
+          </Text>
+        </View> : null}
+        {list.map((button, bIndex) => {
+          let {index, title, color, fontWeight, callback} = button;
+          let borderBottomWidth = Number(bIndex !== list.length - 1);
+
+          return (
+            <TouchableOpacity
+              style={[styles.button, {borderBottomWidth}]}
+              key={'button_' + button.index}
+              onPress={this.onButtonPress.bind(this, index, callback)}
+              activeOpacity={0.9}>
+              <Text style={[styles.buttonText, {color, fontWeight}]}>
+                {title}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  }
+
+  onButtonPress (index, callback) {
+    callback(index);
+    this.hide();
+  }
+
+  onMaskPress () {
+    let {sheets} = this.state;
+    let lastSheet = sheets.slice(-1)[0] || {};
+    let {opts, callback} = lastSheet;
+
+    if (!opts || !callback) {
+      return;
     }
 
-    onButtonPress(index, callback){
-        callback(index);
-        this.hide();
+    let {options = [], cancelButtonIndex, destructiveButtonIndex} = lastSheet.opts;
+
+    if (cancelButtonIndex < options.length && cancelButtonIndex !== destructiveButtonIndex) {
+      callback(cancelButtonIndex);
+      this.hide();
     }
-
-    onMaskPress(){
-        let {sheets} = this.state;
-        let lastSheet = sheets.slice(-1)[0] || {};
-        let {opts, callback} = lastSheet;
-
-        if(!opts || !callback){
-            return
-        }
-
-        let {options=[], cancelButtonIndex, destructiveButtonIndex} = lastSheet.opts;
-
-        if(cancelButtonIndex < options.length && cancelButtonIndex !== destructiveButtonIndex){
-            callback(cancelButtonIndex);
-            this.hide();
-        }
-    }
+  }
 }
 
 /**
@@ -203,24 +212,24 @@ export default class ActionSheet extends Component {
  * - `tintColor` (string) - button color
  */
 ActionSheet.showActionSheetWithOptions = (
-    opts,
-    callback
+  opts,
+  callback
 ) => {
-    if(isIOS){
-        // IOS 直接调用API
-        ActionSheetIOS.showActionSheetWithOptions(opts, callback)
-    }else{
-        // Android
-        if(global.__action_sheet instanceof ActionSheet){
-            global.__action_sheet.show(opts, callback);
-        }else{
-            throw 'ActionSheet has not been initialized. To initialize ActionSheet, you should put `<ActionSheet />` to your render().';
-        }
+  if (isIOS) {
+    // IOS 直接调用API
+    ActionSheetIOS.showActionSheetWithOptions(opts, callback);
+  } else {
+    // Android
+    if (global.__action_sheet instanceof ActionSheet) {
+      global.__action_sheet.show(opts, callback);
+    } else {
+      throw Error('ActionSheet has not been initialized. To initialize ActionSheet, you should put `<ActionSheet />` to your render().');
     }
+  }
 };
 
 ActionSheet.hide = () => {
-    global.__action_sheet.hide();
+  global.__action_sheet.hide();
 };
 
 global.ActionSheet = ActionSheet;
